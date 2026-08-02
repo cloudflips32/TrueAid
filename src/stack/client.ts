1396734fb9a2d95c015c8b4acc953e8941c7ac52
Adam Standish <adamjstandishcoder@gmail.com>
2026-08-02 02:01:16 -0400

    stack client edit

--- src/stack/client.ts
+++ src/stack/client.ts
@@ -3,7 +3,6 @@ import { StackClientApp } from "@stackframe/react";
 
 export const stackClientApp = new StackClientApp({
   projectId: import.meta.env.VITE_STACK_PROJECT_ID,
-  publishableClientKey: import.meta.env.VITE_STACK_PUBLISHABLE_CLIENT_KEY,
   tokenStore: "cookie",
   redirectMethod: { useNavigate },
 });
